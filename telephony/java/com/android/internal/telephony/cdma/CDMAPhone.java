aaed9b7fe746328a8cec8caa5357a975aa04d23d
Naveen Kalla <nkalla@codeaurora.org>
2010-03-17 10:40:16 -0700

    Support for User to User Signaling (UUS)
    Enabling passing UUS information during MO and MT calls.
    
    Change-Id: I31621c0a9d3c0607d99d18c49bb6c593cadd0327

--- telephony/java/com/android/internal/telephony/cdma/CDMAPhone.java
+++ telephony/java/com/android/internal/telephony/cdma/CDMAPhone.java
@@ -62,6 +62,7 @@ import com.android.internal.telephony.PhoneProxy;
 import com.android.internal.telephony.PhoneSubInfo;
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
+import com.android.internal.telephony.UUSInfo;
 
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_ALPHA;
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_NUMERIC;
@@ -342,6 +343,10 @@ public class CDMAPhone extends PhoneBase {
         // Need to make sure dialString gets parsed properly
         String newDialString = PhoneNumberUtils.stripSeparators(dialString);
         return mCT.dial(newDialString);
+    }
+
+    public Connection dial(String dialString, UUSInfo uusInfo) throws CallStateException {
+        throw new CallStateException("Sending UUS information NOT supported in CDMA!");
     }
 
     public SignalStrength getSignalStrength() {
@@ -1405,5 +1410,4 @@ public class CDMAPhone extends PhoneBase {
         }
         return false;
     }
-
 }
